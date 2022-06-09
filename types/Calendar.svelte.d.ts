8dbda730ff4020952b1553cc66eece3121ae7604
George <george@icetech.com.tw>
2022-06-09 10:13:19 +0800

    Update types

--- types/Calendar.svelte.d.ts
+++ types/Calendar.svelte.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="svelte2tsx/svelte-shims" />
 export default class Calendar__SvelteComponent_ extends Svelte2TsxComponent<Partial<{
+    date?: Date;
     class?: string;
 }>, {
     SelectedDate: CustomEvent<any>;
